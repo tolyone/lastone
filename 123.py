339fd39f8105fc256dce0bb93854a82910786a59
tolyone <tolybook@gmaill.com>
2018-12-28 14:22:02 +0300

    new1

--- 123.py
+++ 123.py
@@ -1,18 +1,19 @@
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 x = []
 y = []
 z = []
-list = []
+L = []
 
 for e in os.listdir('0.0013 MeV'):
-    list.append('0.0013 MeV' + '/' + e)
+    L.append('0.0013 MeV' + '/' + e)
     e = e.split()
-    y.append(float(e[0]))
+    z.append(float(e[0]))
 
-for name in list:
+for name in L:
     f = open(name, 'r')
     i = 0
     for e in f:
@@ -21,12 +22,31 @@ for name in list:
             i += 1
         elif e.__len__() == 2 and i == 1:
             x.append(e[0])
-            z.append(e[1])
+            y.append(e[1])
 
         if e.__len__() == 1 and str(e[0]) == '1e+11':
             i -= 1
 
+fig = plt.figure(figsize=(8, 3))
+ax1 = fig.add_subplot(121, projection='3d')
+ax1 = fig.add_subplot(122, projection='3d')
 
+_x = np.array([xi for xi in x])
+_y = np.array([yi for yi in y])
+_xx, _yy = np.meshgrid(_x, _y)
+X, Y = _xx.ravel(), _yy.ravel()
+
+for e in range(100,10001):
+    z.append(z[e-100])
+
+top = z
+bottom = np.zeros_like(top)
+width = depth = 1
+
+ax1.bar3d(X, Y, bottom, width, depth, top, shade=True)
+ax1.set_title('Volumed')
+
+plt.show()
 print(x.__len__())
 print(z.__len__())
 print(y.__len__())
